ec99f99fdd831d4324259d3700d4f7989c8ba053
Pablo <pabloguibe01@gmail.com>
2023-03-01 14:18:15 +0100

    dia 1

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// código PHP aquí
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Libreria</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- enlaces a archivos CSS y JS aquí -->
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="index.php">Inicio</a></li>
+			<li><a href="catalogodelibros.php">Catalogo de libros</a></li>
+			<li><a href="#">Servicios</a></li>
+			<li><a href="#">Contacto</a></li>
+		</ul>
+	</nav>
+	<!-- contenido de la página aquí -->
+</body>
+</html>
